f807450bf58d8549de3d6905b4ab51e93760c258
1NP <nueng.lifestyle@gmail.com>
2024-06-04 19:00:27 +0700

    Connect with MongoDB Atlas

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -28,8 +28,10 @@ exports.getHospital= async (req,res,next) => {
 exports.createHospital=async (req,res,next) => {
 try{
     const hospital = await Hospital.create(req.body);
+    //console.log(req.body);
     res.status(201).json({
         success:true,
+        //msg:"Create new Hospitals",
         data:hospital
     });
 }
@@ -57,6 +59,15 @@ exports.updateHospital=async (req,res,next) => {
     }
 }
 
-exports.deleteHospital=(req,res,next) => {
-    res.status(200).json({success:true,msg:`Delete hospital ${req.params.id}`});
+exports.deleteHospital=async (req,res,next) => {
+    try{
+        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        if(!hospital)
+            res.status(400).json({success:false});
+
+        res.status(200).json({success:true,data :{}});
+    }catch(err){
+        res.status(400).json({success:false});
+    }
+    
 }
